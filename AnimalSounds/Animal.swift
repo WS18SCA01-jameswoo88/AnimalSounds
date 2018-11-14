45349d7596a6f28bdc72b9aee6765b1b883aaef5
James Chun <jameswchun@gmail.com>
2018-11-14 11:59:28 -0500

    better with a structure

---
+++ AnimalSounds/Animal.swift
@@ -0,0 +1,15 @@
+//
+//  Animal.swift
+//  AnimalSounds
+//
+//  Created by James Chun on 11/14/18.
+//  Copyright © 2018 James Chun. All rights reserved.
+//
+
+import Foundation
+
+struct Animal {
+    let emoji: String
+    let exclamation: String
+    let simpleSound: SimpleSound
+}
--- AnimalSounds/ViewController.swift
+++ AnimalSounds/ViewController.swift
@@ -11,28 +11,37 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var animalSoundLabel: UILabel!
-    let meowSound = SimpleSound(named:"meow")
-    let woofSound = SimpleSound(named:"woof")
-    let mooSound = SimpleSound(named:"moo")
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    let animals: [Animal] = [
+        Animal(emoji: "🐱", exclamation: "Meow!", simpleSound: SimpleSound(named: "meow")),
+        Animal(emoji: "🐶", exclamation: "Woof!", simpleSound: SimpleSound(named: "woof")),
+        Animal(emoji: "🐮", exclamation: "Moo!",  simpleSound: SimpleSound(named: "moo"))
+    ];
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    @IBAction func catButtonTapped(_ sender: Any) {
-        animalSoundLabel.text = "Meow!"
-        meowSound.play()
+        //Change the font size of the segmented control to 72.
+        let font: UIFont = UIFont.systemFont(ofSize: 72);
+        let attributes: [NSAttributedString.Key: Any] = [NSAttributedString.Key.font: font]; //a dictionary
+        segmentedControl!.setTitleTextAttributes(attributes, for: UIControl.State.normal);
+        
+        //Put the animals into the segmented control.
+        segmentedControl!.removeAllSegments();
+        for animal in animals {
+            segmentedControl!.insertSegment(withTitle: animal.emoji, at: animals.count, animated: false);
+        }
+        
+        //When the app is launched, no segment is selected yet.
+        segmentedControl!.selectedSegmentIndex = -1;
     }
     
-    @IBAction func dogButtonTapped(_ sender: Any) {
-        animalSoundLabel.text = "Woof!"
-        woofSound.play()
-    }
-    
-    @IBAction func cowButtonTapped(_ sender: Any) {
-        animalSoundLabel.text = "Moo!"
-        mooSound.play()
+    @IBAction func segmentedControlValueChanged(_ sender: Any) {
+        let i: Int = segmentedControl!.selectedSegmentIndex; //Find out which segment was pressed.
+        let animal: Animal = animals[i];
+        animalSoundLabel.text = animal.exclamation;
+        animal.simpleSound.play();
     }
     
 }
